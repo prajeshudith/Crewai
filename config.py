47bd0306dc070416be1e57b153b63b29350e0618
Prajesh <prajesh.chess2000@gmail.com>
2024-05-01 22:59:20 +0530

    removed hf token

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 API_URL = "https://api-inference.huggingface.co/models/Isotonic/distilbert_finetuned_ai4privacy_v2"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": f"Bearer {os.getenv('HF_TOKEN')}"}
